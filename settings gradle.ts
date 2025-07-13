e7c4ff306ded38cf9c471a1a9ea7635679c71ac2
Dev-Re2906 <jalilirad2906@gmail.com>
2025-07-13 14:47:49 +0330

    Create settings gradle.ts

---
+++ settings gradle.ts
@@ -0,0 +1,9 @@
+dependencyResolutionManagement {
+		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+		repositories {
+			mavenCentral()
+			maven { url = uri("https://jitpack.io") }
+		}
+}
+
+	:dependencies [[com.github.Dev-Re2906/awesome-tact "Tag"]]	
